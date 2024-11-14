c0a9fa1c30de16fd0df23c90cf6cf066c6ade5c1
Wilson <139521392+wyu4@users.noreply.github.com>
2024-11-14 17:20:09 -0500

    Use trig to determine if player is clicking

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 print('\n------------------------------------')
 
 print('Making imports...')
-import numpy as np
 import cv2
 import mediapipe as mp
 import colors as col
@@ -24,7 +23,9 @@ hands = mpHands.Hands(static_image_mode=False,
                       min_detection_confidence=0.5,
                       min_tracking_confidence=0.5)
 index_tip = 8
-thumb_tip = 4
+click_lm_1 = 12
+click_lm_2 = 4
+winname = 'Handcam'
 mpDraw = mp.solutions.drawing_utils
 
 print('------------------------------------')
@@ -64,24 +65,32 @@ while True:
                 print(f'{col.OKGREEN}{client.PROCESS_NAME} found.{col.ENDC}')
 
         index_pose = (w, h)
-        thumb_pose = (w, h)
+        click_pose_left = (0, h*2)
+        click_pose_right = (w, h*2)
 
         results = hands.process(frame)
         if results.multi_hand_landmarks:
             for handLms in results.multi_hand_landmarks:
                 lms = handLms.landmark
                 lmIndex = lms[index_tip]
-                lmThumb = lms[thumb_tip]
+                lm_click_1 = lms[click_lm_1]
+                lm_click_2 = lms[click_lm_2]
                 if lmIndex:
                     index_pose = (int(lmIndex.x*w), int(lmIndex.y*h))
-                if lmThumb:
-                    thumb_pose = (int(lmThumb.x*w), int(lmThumb.y*h))
-
+                if lm_click_1 and lm_click_2:
+                    if lm_click_1.x < lm_click_2.x:
+                        click_pose_left = (int(lm_click_1.x*w), int(lm_click_1.y*h))
+                        click_pose_right = (int(lm_click_2.x*w), int(lm_click_2.y*h))
+                    else:
+                        click_pose_left = (int(lm_click_2.x*w), int(lm_click_2.y*h))
+                        click_pose_right = (int(lm_click_1.x*w), int(lm_click_1.y*h))
+                        
         line_col = (255, 255, 255)
 
         if hwnd:
+            
             cv2.putText(frame, 'Connected to GD', (0, int(h*0.9)), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 0), 1)
-            if index_pose[1] > thumb_pose[1]:
+            if ((click_pose_left[0] - click_pose_right[0]) * (index_pose[1] - click_pose_right[1]) - (click_pose_left[1] - click_pose_right[1]) * (index_pose[0] - click_pose_right[0])) < 0:
                 if not is_jumping:
                     client.jump(hwnd)
                 is_jumping = True
@@ -95,21 +104,21 @@ while True:
             cv2.putText(frame, 'Not connected to GD', (0, int(h*0.9)), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
 
         cv2.circle(frame, index_pose, 3, (255, 255, 255), -1)
-        cv2.line(frame, index_pose, thumb_pose, line_col, 2)
-        cv2.line(frame, (index_pose[0]-(w//10), thumb_pose[1]), thumb_pose, (255, 255, 255), 1)
+        cv2.line(frame, index_pose, click_pose_left, line_col, 2)
+        cv2.line(frame, index_pose, click_pose_right, line_col, 2)
+        cv2.line(frame, click_pose_left, click_pose_right, (255, 255, 255), 1)
 
         cTime = time.time()
         cv2.putText(frame, f'FPS:{int(1 / (cTime - pTime))}', (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
-        cv2.imshow('Handcam', frame)
+        cv2.imshow(winname, frame)
         pTime = cTime
-        if cv2.waitKey(1) == ord('q'):
-            raise KeyboardInterrupt()
+
+        if cv2.waitKey(1) == ord('q') or not cv2.getWindowProperty(winname, cv2.WND_PROP_VISIBLE):
+            break
     except KeyboardInterrupt:
         print('------------------------------------')
         if waiting_for_process:
-            print(f'{col.WARNING}Aborting. Process not found.{col.ENDC}')
-        else:
-            print(f'{col.WARNING}Quitting out of the program.{col.ENDC}')
+            print(f'{col.WARNING}Aborting...{col.ENDC}')
         break
 cv2.destroyAllWindows()
 cap.release()
